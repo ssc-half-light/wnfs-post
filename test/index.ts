70972369d991c69a14dea51f4d045b0569f135af
nick thomas <nichoth@gmail.com>
2023-03-13 13:14:42 -0700

    can compile tests

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,20 @@
-const { test } = require('tapzero')
-const { WnfsBlobs } = require('../src/index.js')
+import { test } from 'tapzero'
+import { WnfsBlobs } from '../src/index.js'
+import * as wn from 'webnative'
 
 test('make a post', async t => {
+    const APP_INFO = { name: 'test', creator: 'test' }
+
+    const program = await wn.program({
+        namespace: APP_INFO
+    })
+
+    if (!program.session?.fs) return
+
+    // constructor ({ APP_INFO, LOG_DIR_PATH, BLOB_DIR_PATH, wnfs }:wnfsBlobsArgs) {
+    const wnfsBlobs = new WnfsBlobs({
+        wnfs: program.session.fs,
+        APP_INFO
+    })
 
 })
