5e3e0e8b20e26bf6b0aab4cbe5d90ebb4b986230
nick thomas <nichoth@gmail.com>
2023-03-16 12:23:25 -0700

    test works

--- test/index.ts
+++ test/index.ts
@@ -1,4 +1,9 @@
 import { test } from 'tapzero'
+import { sha256 } from 'webnative/components/crypto/implementation/browser'
+import { publicKeyToDid } from 'webnative/did/transformers'
+import * as uint8arrays from 'uint8arrays'
+import type { Crypto } from 'webnative'
+
 import { WnfsBlobs } from '../src/index.js'
 // import fs from 'fs'
 // import path from 'path'
@@ -6,6 +11,7 @@ import { WnfsBlobs } from '../src/index.js'
 
 // @ts-ignore
 const wn = window.webnative
+// const { crypto } = wn.components
 
 // const data = fs.readFileSync(path.join(__dirname, 'caracal.jpg'))
 // const blob = new Blob([data as BlobPart])
@@ -22,22 +28,45 @@ test('make a post', async t => {
     const program = await wn.program({
         namespace: APP_INFO
     })
+    const { crypto } = program.components
+    const username = await prepareDid(await createDID(crypto))
+
+    // *must* call `register` before we use the `session`
+    await program.auth.register({ username })
 
     const { keystore } = program.components.crypto
 
-    if (!program.session?.fs) return
+    const session = program.session ?? await program.auth.session()
 
     // constructor ({ APP_INFO, LOG_DIR_PATH, BLOB_DIR_PATH, wnfs }:wnfsBlobsArgs) {
     const wnfsBlobs = new WnfsBlobs({
-        wnfs: program.session.fs,
+        wnfs: session.fs,
         APP_INFO
     })
 
     const res = await wnfsBlobs.post(keystore, file, {
         text: 'testing',
-        author: ''
+        author: 'abc'
     })
 
+    console.log('resssssssssssss', res)
+
     t.ok(res.signature)
-    console.log('res', res)
 })
+
+async function prepareDid (did:string): Promise<string> {
+    const normalizedDid = did.normalize('NFD')
+    const hashedUsername = await sha256(
+        new TextEncoder().encode(normalizedDid)
+    )
+
+    return uint8arrays.toString(hashedUsername, 'base32').slice(0, 32)
+}
+
+const createDID = async (
+    crypto: Crypto.Implementation
+): Promise<string> => {
+    const pubKey = await crypto.keystore.publicExchangeKey()
+    const ksAlg = await crypto.keystore.getAlgorithm()
+    return publicKeyToDid(crypto, pubKey, ksAlg)
+}
