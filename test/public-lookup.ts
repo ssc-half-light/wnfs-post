6e777d31d8d2640a613dac4bd1fb31e6b5c20eb5
nick thomas <nichoth@gmail.com>
2023-05-05 15:07:07 -0700

    wip

---
+++ test/public-lookup.ts
@@ -0,0 +1,11 @@
+import { test } from 'tapzero'
+
+//
+// we are storing profile data in the DB
+// and profile pictures in cloudinary
+// so no need for odd-sdk here
+//
+
+test('get public profile', t => {
+
+})
